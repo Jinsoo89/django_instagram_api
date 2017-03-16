d32723619f932c3709c8a78bf37418c2f5c9a12c
Jinsoo89 <hello.python89@gmail.com>
2017-03-16 15:16:03 +0900

    Test create_post 구현

--- django_app/post/tests/tests_apis.py
+++ django_app/post/tests/tests_apis.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APILiveServerTestCase
@@ -12,6 +11,7 @@ User = get_user_model()
 class PostTest(APILiveServerTestCase):
     test_username = 'jinsoo'
     test_password = 'test_password'
+    test_created_date = '2017.01.01'
 
     def create_user(self):
         user = User.objects.create_user(
@@ -20,12 +20,42 @@ class PostTest(APILiveServerTestCase):
         )
         return user
 
-    def test_create_post(self):
+    def create_post(self):
         # Post.objects.create(
-        #     author=user,
-        #     photo=
+        #     author=self.create_user(),
+        #     created_date=self.test_created_date,
         # )
-        pass
+
+        url = reverse('post-create')
+        data = {
+            'author': self.test_username,
+            'created_date': self.test_created_date,
+        }
+        response = self.client.post(url, data, format='json')
+        print(response.status_code)
+        return response
+
+    def test_create_post(self):
+        # Post를 만들 유저를 생성 및 로그인
+        user = self.create_user()
+        self.client.login(username=self.test_username,
+                          password=self.test_password)
+        # Post를 생성하는 API주소를 reverse
+        url = reverse('post-create')
+        # Post를 생성하는 API주소에 POST요청, response를 받아옴
+        response = self.create_post(url)
+        print(response.status_code)
+
+        # response의 status_code가 201(created)이어야 함
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # 생성 후 Post인스턴스가 총 한개어여함
+        self.assertEqual(Post.objects.count(), 1)
+        # 생성된 Post인스턴스의 author pk(id)가 테스트시 생성한 User의 pk(id)와 같아야함
+        post = Post.objects.first()
+        self.assertEqual(post.author.id, user.id)
+        # self.assertEqual(response.data.get('author'), self.test_username)
+        # self.assertEqual(response.data.get('created_date'),
+        #                  self.test_created_date)
 
     def test_cannot_create_post_not_authenticated(self):
         url = reverse('post-create')
