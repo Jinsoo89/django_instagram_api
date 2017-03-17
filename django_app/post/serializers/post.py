f57a738726dc1cf264dd1c10de8cb49c8269e0f2
Jinsoo89 <hello.python89@gmail.com>
2017-03-17 14:48:17 +0900

    userserializer 추가 및 postserializer에 연결

--- django_app/post/serializers/post.py
+++ django_app/post/serializers/post.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from member.serializers import UserSerializer
 from post.models import Post
 
 __all__ = (
@@ -9,7 +10,6 @@ __all__ = (
 
 
 class PostSerializer(serializers.ModelSerializer):
-    # author = serializers.PrimaryKeyRelatedField(read_only=True)
     """
     author 필드의 값이 pk가 아닌 하나의 Object(dict)로 나타나도록 수정
     1. 관련 테스트코드 작성
@@ -17,8 +17,7 @@ class PostSerializer(serializers.ModelSerializer):
     2. 해당 테스트코드가 작동하도록 PostSerializer를 수정 및 UserSerializer 작성
     3. 테스트 및 포스트맨 작동 확인
     """
-
-    # user = UserSerializer(required=False)
+    author = UserSerializer(read_only=True)
 
     class Meta:
         model = Post
@@ -28,6 +27,5 @@ class PostSerializer(serializers.ModelSerializer):
             'created_date',
         )
         read_only_fields = (
-            'author',
             'created_date',
         )
