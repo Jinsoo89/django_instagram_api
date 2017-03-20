007cd8038aaaf3960102429795bbdc2cf167ddfb
Jinsoo89 <hello.python89@gmail.com>
2017-03-20 17:28:11 +0900

    include를 사용해 template파일들 code-refactoring

--- django_app/post/views/post.py
+++ django_app/post/views/post.py
@@ -10,8 +10,9 @@
         TEMPLATE_DIR 변수 설정 TEMPLATE설정의 TEMPLATE_DIRS에 해당 변수 추가
     5. PostList CBV에 get메서드 작성 및 내부 쿼리를 return
 """
-from django.shortcuts import render
 from django.views import View
+from django.views.generic import DetailView
+from django.views.generic import ListView
 
 from post.models import Post
 
@@ -22,24 +23,27 @@ __all__ = (
 )
 
 
-class PostList(View):
+class PostList(ListView):
     """
     1. 데이터추가
         Postman으로 Post두개 만들고 각각의 Post에 PostPhoto를 3개 추가
     2. post_list.html에서 posts변수를 loop하며 각 post의 postphoto_set.all을 loop
         postphoto_set을 내부에서 loop하며 내부 loop아이템의 photo.url을 이용해 이미지 출력
     """
-
-    def get(self, request):
-        posts = Post.objects.all()
-        context = {
-            'posts': posts,
-        }
-        return render(request, 'post/post-list.html', context)
+    model = Post
+    context_object_name = 'posts'
 
 
-class PostDetail(View):
-    pass
+class PostDetail(DetailView):
+    """
+    DetailView를 상속받아서 구현되도록
+    """
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data(**kwargs)
+        context['post_list'] = Post.objects.all()
+        return context
 
 
 class PostDelete(View):
