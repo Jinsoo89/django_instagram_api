3910ff29fb67e35a1e751ef119f400ce8e74d235
Jinsoo89 <hello.python89@gmail.com>
2017-03-21 15:45:37 +0900

    post view의 PostDelete작성 및 부가 내용 추가

--- django_app/post/views/post.py
+++ django_app/post/views/post.py
@@ -10,13 +10,15 @@
         TEMPLATE_DIR 변수 설정 TEMPLATE설정의 TEMPLATE_DIRS에 해당 변수 추가
     5. PostList CBV에 get메서드 작성 및 내부 쿼리를 return
 """
-from django.shortcuts import render
-from django.views import View
+from django.urls import reverse_lazy
+from django.views.generic.edit import DeleteView
 from django.views.generic import DetailView
+from django.views.generic import FormView
 from django.views.generic import ListView
 
 from post.forms import PostForm
-from post.models.post import Post
+from post.models.comment import PostComment
+from post.models.post import Post, PostPhoto
 
 __all__ = (
     'PostList',
@@ -37,29 +39,75 @@ class PostList(ListView):
     context_object_name = 'posts'
 
 
-class PostCreate(View):
+class PostCreate(FormView):
     """
-    PostForm을 사용
-        fields
-            content
-            photos
-    1. Post요청을 받았을때, 해당 request.user를 author로 하는 Post 인스턴스 생성
-    2. 만약 form.cleaned_data['content']가 빈 값이 아니면 PostComment 인스턴스 생성
-    3. request.FILES.getlist('photos')를 loop하며 PostPhoto 인스턴스 생성
-    4. return redirect('post:post-list')
+    FormView를 상속받아 구현
+    attributes
+        template_name
+        form_class
+        success_url
+    method
+        form_valid(self, form)
+            request는 self.request로 접근 가능
+            form.cleaned_data는 바로 사용 가능
+            redirect는 정의해줄 필요 없음
     """
     form_class = PostForm
     template_name = 'post/post_create.html'
+    success_url = '/post/'
 
-    def get(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        context = {
-            'form': form,
-        }
-        return render(request, self.template_name, context)
+    def form_valid(self, form):
+        post = Post.objects.create(author=self.request.user)
+        content = form.cleaned_data.get('content').strip()
+        if content is not None:
+            PostComment.objects.create(
+                post=post,
+                author=self.request.user,
+                content=content,
+            )
+        photos = self.request.FILES.getlist('photos')
+        for photo in photos:
+            PostPhoto.objects.create(
+                post=post,
+                photo=photo,
+            )
+        return super(PostCreate, self).form_valid(form)
 
-    def post(self):
-        pass
+        # form_class = PostForm
+        # template_name = 'post/post_create.html'
+        #
+        # def get(self, request, *args, **kwargs):
+        #     form = self.form_class(request.POST)
+        #     context = {
+        #         'form': form,
+        #     }
+        #     return render(request, self.template_name, context)
+        #
+        # def post(self, request, *args, **kwargs):
+        #     form = self.form_class(request.POST, request.FILES)
+        #     # 해당 request.user를 author로 하는 Post인스턴스 생성
+        #     if form.is_valid():
+        #         post = Post.objects.create(author=request.user)
+        #         # 2. 만약 form.cleaned_data['content']가 빈 값이 아니면 PostComment 인스턴스 생성
+        #         content = form.cleaned_data.get('content').strip()
+        #         if content is not None:
+        #             PostComment.objects.create(
+        #                 author=request.user,
+        #                 content=content,
+        #                 post=post,
+        #             )
+        #         # 3. request.FILES.getlist('photos')를 loop하며 PostPhoto 인스턴스 생성
+        #         photos = request.FILES.getlist('photos')
+        #         for photo in photos:
+        #             PostPhoto.objects.create(
+        #                 post=post,
+        #                 photo=photo,
+        #             )
+        #         # 4. return redirect('post:post-list')
+        #         return redirect('post:post-list')
+        #     else:
+        #         return HttpResponse(form.errors)
+
 
 class PostDetail(DetailView):
     """
@@ -73,5 +121,6 @@ class PostDetail(DetailView):
         return context
 
 
-class PostDelete(View):
-    pass
+class PostDelete(DeleteView):
+    model = Post
+    success_url = reverse_lazy('post:post-list')
